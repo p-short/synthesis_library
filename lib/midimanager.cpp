475129c8aedf7695b9b7ca4318df4d2ca1151824
Phillip Short <phillip.short@hotmail.co.uk>
2025-09-23 21:00:37 +0100

    adding rtmidi wrapper

---
+++ lib/midimanager.cpp
@@ -0,0 +1,48 @@
+#include "midimanager.h"
+#include "RtMidi.h"
+
+namespace musiclib {
+
+    MidiManager::~MidiManager() = default;
+
+    MidiManager::MidiManager() {
+        m_midiIn = std::make_unique<RtMidiIn>();
+    }
+
+    MidiManager& MidiManager::GetInstance() {
+        static MidiManager instance;
+        return instance;
+    }
+
+    uint32_t MidiManager::GetDeviceCount() {
+        return m_midiIn->getPortCount();
+    }
+
+    std::string MidiManager::GetDeviceName(const uint32_t index) {
+        return m_midiIn->getPortName(index);
+    }
+
+    bool MidiManager::ConnectDevice(const uint32_t index) {
+        try {
+            m_midiIn->openPort(index);
+            return true;
+        } catch (const std::exception& ) {
+            return false;
+        }
+    }
+
+    void MidiManager::AssignCallback(std::function<void(double, const std::vector<uint8_t>&)> callback) {
+        m_userCallback = std::move(callback);
+        // Set RtMidi callback to the static wrapper
+        m_midiIn->setCallback(&MidiManager::Callback, this);
+        // Don't ignore any messages
+        m_midiIn->ignoreTypes(false, false, false);
+    }
+
+    void MidiManager::Callback(double deltatime, std::vector<uint8_t>* message, void* userData) {
+        auto* manager = static_cast<MidiManager*>(userData);
+        if (manager->m_userCallback && message) {
+            manager->m_userCallback(deltatime, *message);
+        }
+    }
+}
---
+++ lib/midimanager.h
@@ -0,0 +1,26 @@
+#ifndef MUSICLANGUAGE_LIB_MIDIMANAGER_H
+#define MUSICLANGUAGE_LIB_MIDIMANAGER_H
+
+#include <memory>
+#include <functional>
+
+class RtMidiIn;
+
+namespace musiclib {
+    class MidiManager {
+    public:
+        ~MidiManager();
+        static MidiManager& GetInstance();
+        uint32_t GetDeviceCount();
+        std::string GetDeviceName(const uint32_t index);
+        bool ConnectDevice(const uint32_t index);
+        void AssignCallback(std::function<void(double, const std::vector<uint8_t>&)> callback);
+    private:
+        MidiManager();
+        std::function<void(double, const std::vector<uint8_t>&)> m_userCallback;
+        static void Callback(double deltatime, std::vector<uint8_t>* message, void* userData);
+        std::unique_ptr<RtMidiIn> m_midiIn { nullptr };
+    };
+}
+
+#endif // MUSICLANGUAGE_LIB_MIDIMANAGER_H
