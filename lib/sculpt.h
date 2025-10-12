3f7c9f993ea5796364607f67b742b02c516aace8
Phillip Short <phillip.short@hotmail.co.uk>
2025-10-12 17:48:41 +0100

    changing musiclib to Sculpt in source files.

---
+++ lib/sculpt.h
@@ -0,0 +1,17 @@
+#ifndef MUSICLANGUAGE_LIB_SCULPT_H
+#define MUSICLANGUAGE_LIB_SCULPT_H
+
+#include "soundsource.h"
+#include "sinewave.h"
+#include "trianglewave.h"
+#include "squarewave.h"
+#include "sawwave.h"
+#include "noise.h"
+#include "audioengine.h"
+#include "midimanager.h"
+#include "envelope.h"
+#include "utility.h"
+#include "sampleplayer.h"
+#include "clock.h"
+
+#endif // MUSICLANGUAGE_LIB_MUSICLANGUANGE_H
